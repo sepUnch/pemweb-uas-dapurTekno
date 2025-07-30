6488320487ffcbd71c7c4c0875f0fc69d29021b9
Fahri Arkan <fahrizulfadli14@gmail.com>
2025-07-30 15:06:39 +0700

    deploy

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanner("c4480250-2c19-4ebe-91f9-4d51cc996fb4");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produk Ungulan" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
